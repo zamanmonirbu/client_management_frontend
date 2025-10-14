cb215d7b36212f42fedd7984af223d398fc6ed87
MD. MONIRUZZAMAN <monir.cse6.bu@gmail.com>
2025-10-14 21:19:28 +0600

    Remove pnpm-lock and fix basic issues

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['moniruzzaman.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'moniruzzaman.com',
+      },
+    ],
   },
 }
 
